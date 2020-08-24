b448c44769612c86af6fc20a8eef6f0997293d6a
Egor Devyanin <38373062+Bakalai@users.noreply.github.com>
2020-08-24 19:02:33 +0700

    Add files via upload

---
+++ test-1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Тестовое задание</h1>
+    <LoadDataJson/>
+  </div>
+</template>
+
+<script>
+import LoadDataJson from './components/LoadDataJson.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoadDataJson
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ test-1/src/components/LoadDataJson.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <table>
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Phone</th>
+            </tr>
+            </thead>
+            <tr v-bind:key="todo.id" v-for="todo in userData">
+                <td> {{ todo.id }} </td>
+                <td> {{ todo.name }} </td>
+                <td> {{ todo.age }} </td>
+                <td> {{ todo.phone }} </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Table load',
+        data() {
+            return {
+                userData: []
+            }
+        },
+        props: {},
+        methods: {
+            loadData: function () {
+                fetch("data.json")
+                    .then(response => response.json())
+                    .then(data => (this.userData = data));
+            }
+        },
+        mounted() {
+            this.loadData();
+        },
+    }
+</script>
+
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
